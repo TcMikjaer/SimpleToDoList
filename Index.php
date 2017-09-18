ccf9fa49d4d09e7d6b65b34eaca5b2b7c10dc8d6
TcMikjaer <tc@mikjaer.com>
2017-09-19 01:21:41 +0200

    Refactoring all the _GET inputs to a single variable called action. Change some of the form inputs to buttons. Made all database calls use the Query class.

--- Index.php
+++ Index.php
@@ -7,12 +7,57 @@ $tasks = $toDoList->getTasks();
 
 ?>
 
-<div class="container">
+<!--<div class="container">
     <div class="row">
         <form method="GET">
             <label>Task Description</label>
             <input type="text" name="description" id="TaskDescriptionInput">
             <input type="submit" value="Create Task" class="btn btn-success" name="CREATE">
+        </form> 
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Tasks</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                //<?php 
+//                foreach ($tasks as &$task) 
+//                {
+//                    echo "<tr>";
+//                    echo "<form method='GET'>";
+//                    if ($task->id == $toDoList->UPDATEID)
+//                    {
+//                        echo "<td><input type='text' value='". $task->description . "' name='descriptionUpdated'></td>";
+//                        echo "<td><input type='submit' value='Confirm Update' class='btn btn-success' name='UPDATE'></td>";
+//                    }
+//                    else 
+//                    {
+//                        echo "<td>". $task->description . "</td>";
+//                        echo "<td><input type='submit' value='Delete' class='btn btn-danger' name='DELETE'><input type='submit' value='Update' class='btn btn-warning' name='INITUPDATE'></td>";
+//                    }
+//                    echo '<td><input type="hidden" value="' . $task->id . '" class="btn btn-danger" name="id"></td>';
+//                    echo "</form>";
+//                    echo "</tr>";
+//                }
+//                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+</body>
+</html>-->
+
+
+
+<div class="container">
+    <div class="row">
+        <form method="GET">
+            <label>Task Description</label>
+            <input type="text" name="description" id="TaskDescriptionInput">
+            <input type="submit" value="Create Task" class="btn btn-success">
+            <input type="hidden" name="action" value="create">
         </form> 
         <table class="table table-striped table-bordered">
             <thead>
@@ -30,14 +75,15 @@ $tasks = $toDoList->getTasks();
                     if ($task->id == $toDoList->UPDATEID)
                     {
                         echo "<td><input type='text' value='". $task->description . "' name='descriptionUpdated'></td>";
-                        echo "<td><input type='submit' value='Confirm Update' class='btn btn-success' name='UPDATE'></td>";
+                        echo "<td align='right'><button type='submit' name='action' value='update' class='btn btn-success'>Update</button>";
                     }
                     else 
                     {
                         echo "<td>". $task->description . "</td>";
-                        echo "<td><input type='submit' value='Delete' class='btn btn-danger' name='DELETE'><input type='submit' value='Update' class='btn btn-warning' name='INITUPDATE'></td>";
+                        echo "<td align='right'><button type='submit' name='action' value='delete' class='btn btn-danger'>Delete</button> &nbsp;";
+                        echo "<button type='submit' name='action' value='initupdate' class='btn btn-warning'>Update</button></td>";
                     }
-                    echo '<td><input type="hidden" value="' . $task->id . '" class="btn btn-danger" name="id"></td>';
+                    echo '<input type="hidden" name="id" value="' . $task->id . '">';
                     echo "</form>";
                     echo "</tr>";
                 }
--- ToDoList.php
+++ ToDoList.php
@@ -14,64 +14,42 @@ class ToDoList
         $this->DB = new Database();
     }
     
-    public function update() 
+    public function update()
     {
-        if (!empty(filter_input(INPUT_GET, 'CREATE')))
-        {   
-            $test = new Query($this->DB);
-            $test->execute("INSERT INTO tasks(description) VALUES(:description)", [':description' => addslashes(filter_input(INPUT_GET, 'description'))]);
-            header("Location: Index.php");
-            die();
-        }
-        elseif (!empty(filter_input(INPUT_GET, 'DELETE'))) 
+        if (!empty($action = filter_input(INPUT_GET, 'action')))
         {
-            $test = new Query($this->DB);
-            $test->execute("DELETE FROM tasks WHERE id = :id", [':id' => filter_input(INPUT_GET, 'id')]);
-            header("Location: Index.php");
-            die();
-        }
-        elseif (!empty(filter_input(INPUT_GET, 'INITUPDATE'))) 
-        {
-            $this->UPDATEID = filter_input(INPUT_GET, 'id');
-
-        }
-        elseif (!empty(filter_input(INPUT_GET, 'UPDATE'))) 
-        {
-            $test = new Query($this->DB);
-            $test->execute("UPDATE Tasks SET description = :description WHERE id = :id", 
-                [
-                    ':id' => filter_input(INPUT_GET, 'id'),
-                    ':description' => filter_input(INPUT_GET, 'descriptionUpdated')
-                ]
-            );
-            header("Location: Index.php");
-            die();
+            $query = new Query($this->DB);
+            switch ($action) {
+                case "create":
+                    $query->execute("INSERT INTO tasks(description) VALUES(:description)", [':description' => addslashes(filter_input(INPUT_GET, 'description'))]);
+                    break;
+                case "delete":
+                    $query->execute("DELETE FROM tasks WHERE id = :id", [':id' => filter_input(INPUT_GET, 'id')]);
+                    break;
+                case "update":
+                    $query->execute("UPDATE Tasks SET description = :description WHERE id = :id", 
+                        [
+                            ':id' => filter_input(INPUT_GET, 'id'),
+                            ':description' => filter_input(INPUT_GET, 'descriptionUpdated')
+                        ]
+                    );
+                    break;
+            }
+            if($action == "initupdate")
+            {
+                $this->UPDATEID = filter_input(INPUT_GET, 'id');
+            }
+            else 
+            {
+                header("Location: Index.php");
+                die(); 
+            }
         }
     }
     
     public function getTasks()
     {
-        
-//        $test = new Query($this->DB);
-//        $test->execute("SELECT * FROM Tasks ORDER BY id DESC", null);
-//        return $test;  
-        
-        try 
-        {
-            $PDO = $this->DB->connect();
-            $statement = $PDO->query("SELECT * FROM Tasks ORDER BY id DESC");
-            $statement->execute(null);
-            $result = [];
-            while ($row = $statement->fetch())
-            {
-                array_push($result,new Task($row['id'], $row['description']));
-            } 
-            $this->DB->disconnect();
-            return $result;  
-        }
-        catch(PDOException $e)
-        {
-            echo $e->getMessage();
-        }
+        $test = new Query($this->DB);
+        return $test->execute("SELECT * FROM Tasks ORDER BY id DESC");
     }
 }
